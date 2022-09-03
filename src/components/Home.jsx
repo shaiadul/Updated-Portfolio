5b4394f030cf8cd81ae8122d45a0a2c05258a1c5
shaiadul <devshaiadul69@gmail.com>
2022-09-04 02:42:20 +0600

    add some style for small device

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,16 +13,16 @@ const Home = () => {
       <div className="small-screen hidden">
           <img
             src={HeroImage}
-            alt="profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            alt="Saidul-Pic"
+            className="rounded-full mx-auto w-2/3 md:w-full bg-gradient-to-r from-cyan-500 to-purple-500"
           />
         </div>
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Web Developer
+            I'm a MERN Stack Web Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 2 years of experience building websites and desgining software.
+            I have 1 years of experience building websites and desgining software.
             I love to work on web application using technologies like
             React, Tailwind, Next JS and GraphQL.
           </p>
@@ -32,7 +32,7 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+              className="group text-white font-signature font-bold w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-purple-500 cursor-pointer"
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
