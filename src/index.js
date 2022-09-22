a62147fa9bbc14b9efeda7d1e3d215e576c1effc
shaiadul <devshaiadul69@gmail.com>
2022-09-22 18:54:51 +0600

    finalize

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// md saidul basar updated
+// md saidul basar updated portfolio 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
