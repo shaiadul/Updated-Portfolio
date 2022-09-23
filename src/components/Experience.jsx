2b87ca6da8157160a51c0fa859c9d9961c51944a
shaiadul <devshaiadul69@gmail.com>
2022-09-23 10:21:31 +0600

    end of the title

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -94,15 +94,14 @@ const Experience = () => {
     <div
       name="experience"
       className="bg-gradient-to-b from-gray-800 to-black w-full h-screen experience"
-    >
+    >  
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div className="lg:mt-12">
           <p className="text-4xl font-bold border-double border-b-4 border-purple-500 p-2 inline">
             Experience
           </p>
           <p className="py-6 text-purple-500">These are the technologies I've worked with and used them in my projects.</p>
-        </div>
-
+        </div> {/* End of title */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
