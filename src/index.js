3950ef11c7ca4d28784303c7b65a96dff68ce2fb
shaiadul <devshaiadul69@gmail.com>
2022-09-24 00:06:05 +0600

    version set

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 // md saidul basar updated portfolio 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode> { /* md saidul basar updated portfolio */ }
+  <React.StrictMode> { /* v 18 */ }
     <App />
   </React.StrictMode>
 );
